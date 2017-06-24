91ca572920302b115a1e3dadf7036f04b9440645
Alan Smith <alan@randomsmiths.com>
2017-06-24 09:28:36 -0400

    Merge login and unlock page into the same one

--- python/doorapp.py
+++ python/doorapp.py
@@ -5,31 +5,14 @@ from users import Users
 import argparse
 import time
 
+logFile = open("doorapp.log", "a+", 0)   # 0 is unbuffered
+
 ## For running on Raspberry Pi
 try:
    import RPi.GPIO as GPIO 
    TEST_ONLY = False
 except:
    TEST_ONLY = True
-
-class Cookie(object):
-    def __init__(self, name):
-        self.name = name;
-    def get(self, default=''):
-        result = default
-        try:
-           result = cherrypy.session.get(self.name)
-           if not result:
-              self.set(default)
-              result = default
-        except:
-           self.set(default);
-        return result;
-    def set(self, value):
-        cherrypy.session[self.name] = value
-    def delete(self):
-        cherrypy.session.pop(self.name, None)
-        self.set('', 0)  # Way to delete a cookie is to set it with an expiration time of immediate
 
 class DoorGPIO(object):
     def __init__(self):
@@ -40,8 +23,10 @@ class DoorGPIO(object):
            GPIO.setup(self.pin, GPIO.OUT)
         else:
            print "---- NO GPIO WORKING!!! ---- "
-    def unlock(self):
-        print "Unlocking door"
+    def unlock(self, user):
+        logStr = time.asctime() + ": " + user + " unlocking door"
+        print logStr
+        logFile.write(logStr)
         if not TEST_ONLY:
            GPIO.output(self.pin, GPIO.HIGH)
            time.sleep(5)
@@ -56,39 +41,20 @@ class DoorApp(object):
 
     def template(self, name, **kwargs):
         return self.lookup.get_template(name).render(**kwargs);
-    def show_mainpage(self, username,error=''):
-        return self.template("mainpage.html", username= username, error = error)
-    def show_loginpage(self, error=''):
-        return self.template("login.html", error = error)
+    def show_mainpage(self, error=''):
+        return self.template("mainpage.html", error = error)
     
     @cherrypy.expose
     def index(self):
-        username = Cookie('username').get()
-        if not username:
-            return self.show_loginpage()
-        return self.show_mainpage(username)
+        return self.show_mainpage()
 
     @cherrypy.expose
-    def login(self,username=None,password=None):
+    def unlock(self,username=None,password=None):
         if self.users.verify_password(username, password):
-            Cookie('username').set(self.users.get(username))
-            return self.show_mainpage(username);
+            self.door.unlock(username);
+            return self.show_mainpage("");
         else:
-            return self.show_loginpage("Username and password don't match")
-
-    @cherrypy.expose
-    def logout(self):
-        cherrypy.lib.sessions.expire()
-        return self.show_loginpage('')
-
-    @cherrypy.expose
-    def unlock(self):
-# Check for being removed from access list or not logged in
-        username = self.users.get(Cookie('username').get())
-        if not username:
-            return self.show_loginpage()
-        self.door.unlock()    
-        return self.show_mainpage(username)
+            return self.show_mainpage("Username and password don't match")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="TFI Door Unlocker")
